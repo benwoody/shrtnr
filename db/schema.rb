839d0997606afa00a0217a4528888edfdd6f9b9d
Ben Woodall <mail@benwoodall.com>
2015-03-29 10:12:16 -0700

    add api/v1/links to work with 3rd party services
    This commit adds a JSON API so that 3rd party services can interact with
    our url shortening service.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20150806224559) do
+=======
+ActiveRecord::Schema.define(version: 20150326015328) do
+>>>>>>> add api/v1/links to work with 3rd party services
 
   create_table "links", force: :cascade do |t|
     t.string   "short_url"
@@ -22,7 +26,10 @@ ActiveRecord::Schema.define(version: 20150806224559) do
     t.integer  "user_id"
   end
 
+<<<<<<< HEAD
   add_index "links", ["short_url"], name: "index_links_on_short_url"
+=======
+>>>>>>> add api/v1/links to work with 3rd party services
   add_index "links", ["user_id"], name: "index_links_on_user_id"
 
   create_table "users", force: :cascade do |t|

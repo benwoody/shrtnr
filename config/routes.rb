be2085e651fd4c4c86137c90146e2527892da2f4
Ben Woodall <mail@benwoodall.com>
2015-03-29 10:12:16 -0700

    add api/v1/links to work with 3rd party services
    This commit adds a JSON API so that 3rd party services can interact with
    our url shortening service.

--- config/routes.rb
+++ config/routes.rb
@@ -28,6 +28,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :links, only: [:show, :create]
       resources :users, only: [:show]
+      get 'links/create'
     end
   end
 

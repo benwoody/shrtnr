7f366ac77a2dda8a31e5489935ce7992e17d1a74
Jon Parten <jon.t.parten@nordstrom.com>
2015-04-04 22:50:26 -0700

    Added to create short url integration test
    * Added happy path test for signed in user
    * Added happy path test for guest user

--- spec/features/create_short_url_spec.rb
+++ spec/features/create_short_url_spec.rb
@@ -2,13 +2,45 @@ require 'spec_helper'
 
 describe "creating a short url" do
   context "when not signed in" do
+    let(:user) { create(:user) }
+    let(:url) { "http://www.lifehacker.com" }
+
+    before do
+      visit "/home"
+      fill_in "link_long_url", with: url
+      click_button 'Shorten It!'
+    end
+
+    it "tells the user what the shortened link path is" do
+      expect(page).to have_content /shrt.nr\/[a-zA-Z0-9]{6}/
+    end
+
+    it "tells the user what the shortened link points to" do
+      expect(page).to have_content url
+    end
+
+    it "sends them to the unique link path" do
+      expect(current_path).to match /\/links\/[a-zA-Z0-9]{6}/
+    end
   end
 
   context "when signed in" do
     let(:user) { create(:user) }
+    let(:url) { "http://www.lifehacker.com" }
 
     before do
       login_as user
+      fill_in "link_long_url", with: url
+      click_button 'Shorten It!'
     end
+
+    it "tells the user what the shortened link path is" do
+      expect(page).to have_content /shrt.nr\/[a-zA-Z0-9]{6}/
+    end
+
+    it "tells the user what the shortened link points to" do
+      expect(page).to have_content url
+    end
+
   end
 end

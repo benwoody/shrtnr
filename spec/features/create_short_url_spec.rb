7ea19bd726e3403c8b7a6aced73990550bd47cf8
Tom Underhill <tunderhill@gmail.com>
2015-04-06 21:51:27 -0700

    Added some create short url integration tests

--- spec/features/create_short_url_spec.rb
+++ spec/features/create_short_url_spec.rb
@@ -2,6 +2,15 @@ require 'spec_helper'
 
 describe "creating a short url" do
   context "when not signed in" do
+  	before do
+    	visit "/home"
+      fill_in "long_url", with: "http://foo.com"
+      click_button "Shorten It!"
+  	end
+
+    it "shortening an url displays the short link" do
+      expect(page).to have_content "links to http://foo.com"
+    end
   end
 
   context "when signed in" do
@@ -9,6 +18,18 @@ describe "creating a short url" do
 
     before do
       login_as user
+
+      visit "/home"
+      fill_in "long_url", with: "http://foo.com"
+      click_button "Shorten It!"
+    end
+
+    it "shortening an url displays the short link" do
+      expect(page).to have_content "links to http://foo.com"
+    end
+
+    it "add says that the URL was added" do
+      expect(page).to have_content "URL added"
     end
   end
 end

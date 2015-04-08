f507f9307c6dbd12cf4158a430339fea5ffa6a69
Tom Underhill <tunderhill@gmail.com>
2015-04-07 21:08:16 -0700

    added user creation tests

--- spec/features/create_short_url_spec.rb
+++ spec/features/create_short_url_spec.rb
@@ -8,8 +8,16 @@ describe "creating a short url" do
       click_button "Shorten It!"
   	end
 
-    it "shortening an url displays the short link" do
+    it "shortens an url by displaying the short link" do
       expect(page).to have_content "links to http://foo.com"
+    end
+
+    it "has a sign in link" do
+      expect(page).to have_selector(:link_or_button, 'SIGN IN')
+    end
+
+    it "has a sign up link" do
+      expect(page).to have_selector(:link_or_button, 'SIGN UP')
     end
   end
 
@@ -30,6 +38,18 @@ describe "creating a short url" do
 
     it "add says that the URL was added" do
       expect(page).to have_content "URL added"
+    end
+
+    it "has a logout link" do
+      expect(page).to have_selector(:link_or_button, 'LOGOUT')
+    end
+
+    it "has a dashboard link" do
+      expect(page).to have_selector(:link_or_button, 'DASHBOARD')
+    end
+
+    it "has a settings link" do
+      expect(page).to have_selector(:link_or_button, 'SETTINGS')
     end
   end
 end

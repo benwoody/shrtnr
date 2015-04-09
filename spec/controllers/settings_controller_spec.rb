3945d5214b9c7f9918042624fa5eb38bd0d543c7
Joseph Simpson <jjs0sbw@gmail.com>
2015-04-09 16:44:22 -0700

    updated controller spec

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -3,35 +3,28 @@
 describe SettingsController, type: :controller do
    
   let(:user) { FactoryGirl.create(:user) }
-=begin
-  let(:settings) do
+
+  let(:settings_params) do
     {
       :name => "name",
       :email => "e@m.com"
     }
   end
     
-
-  let(:settings_params) { settings }
-=end
-
-  before(:each) do
-    @settings = { :name => "name", :email => "e@m.com"},
-    @settings_params = { :name => "name", :email => "email" }
-  end
-
   describe "#index" do
     it "settings for signed_in user?" do
       get :index
       expect(response).to be_success
     end
   end
+  before do
+    sign_in(user)
+  end
+
 
   describe "#update" do
     it "is successful" do
-      pending "Need to figure this out"
-    
-      post :update, user: @settings_params
+      post :update, settings: settings_params
       expect(response).to redirect_to(settings_url)
     end
 

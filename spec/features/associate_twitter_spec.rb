9ceb1ea850cceadbc608ebec557ebcc4747b6e5b
GGadow <gpg@gregory-gadow.net>
2015-04-14 21:29:21 -0700

    Saving work

---
+++ spec/features/associate_twitter_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe "Twitter association" do
+
+  context "not yet linked to Twitter" do
+
+    before do
+      visit "/users/new"
+      fill_in "Email", with: "test@test.com"
+      fill_in "Password", with: "Password"
+      fill_in "Password confirmation", with: "Password"
+      click_button "Submit"
+    end
+
+    it "should be able to add a Twitter id" do
+      visit "/settings"
+
+      fill_in "Twitter ID", with: "1234"
+      click_button "Update"
+
+      expect(page).to have_content "Successfully updated settings"
+    end
+
+    it "should have the Twitter user id" do
+      expect(page).to have_selector("input", exact: "1234")
+    end
+
+#    it "should be able to remove a Twitter id" do
+#      visit "/settings" #reset the page
+#
+#      fill_in "Twitter ID", with: ""
+#      click_button "Update"
+#
+#      expect(page).to have_content "Successfully updated settings"
+#    end
+
+#    it "should have the Twitter user id" do
+#      expect(page).to have_selector("input", exact: "")
+#    end
+    
+  end
+end

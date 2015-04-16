f458800894bb9dc8ebcd4c20dc6fb91b04c84bea
Tom Underhill <tunderhill@gmail.com>
2015-04-15 23:29:51 -0700

    fixed add_twitter test in SettingsController test

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -11,12 +11,13 @@ describe SettingsController, type: :controller do
       before do
         OmniAuth.config.add_mock(:twitter, { uid: '12345' })
         request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:twitter]
+        sign_in(user)
       end
 
       it "adds a uid to User" do
         # I expected the session to exist, but it does not
-        # put :add_twitter
-        # puts session[:add_twitter]
+        put :add_twitter
+        expect(session[:add_twitter]).to be_truthy
       end
     end
   end

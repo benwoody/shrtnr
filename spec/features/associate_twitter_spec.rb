c4320f2335139ca1449731be9bfb3386904b53a6
GGadow <gpg@gregory-gadow.net>
2015-04-15 21:18:45 -0700

    Cleaned up testing

--- spec/features/associate_twitter_spec.rb
+++ spec/features/associate_twitter_spec.rb
@@ -12,36 +12,11 @@ describe "Twitter association" do
       click_button "Submit"
     end
 
-    it "should be able to add a Twitter id" do
+    it "should be linkable to Twitter" do
       visit "/settings"
 
-      fill_in "Twitter ID", with: "1234"
-      click_button "Update"
-
-      expect(page).to have_content "Successfully updated settings"
+      expect(page).to have_content "Link To Twitter Account"
     end
-
-    it "should have the Twitter user id" do
-      #expect(page).to have_selector("input", exact: "1234")
-
-      #expect(page).to have_field "settings[uid]"
-      #expect(page).to have_field "Twitter ID"
-      #expect(page).to have_field "settings_uid"
-      expect(page).to have_selector("input#settings_uid")
-    end
-
-#    it "should be able to remove a Twitter id" do
-#      visit "/settings" #reset the page
-#
-#      fill_in "Twitter ID", with: ""
-#      click_button "Update"
-#
-#      expect(page).to have_content "Successfully updated settings"
-#    end
-
-#    it "should have the Twitter user id" do
-#      expect(page).to have_selector("input", exact: "")
-#    end
     
   end
 end

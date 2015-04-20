2c702f81e21eaf8b161bb49e679bda829cf454d9
GGadow <gpg@gregory-gadow.net>
2015-04-19 21:57:32 -0700

    I think this is it

--- spec/mailers/update_mailer_spec.rb
+++ spec/mailers/update_mailer_spec.rb
@@ -1,5 +1,21 @@
-require "rails_helper"
+require "spec_helper"
 
 RSpec.describe UpdateMailer, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+
+  describe "#settings_email" do
+    let(:mail) { UpdateMailer.settings_email(user, user) }
+
+    it "renders the correct headers " do
+      expect(mail.from).to include "notifications@shrt.nr"
+      expect(mail.to).to include user.email
+      expect(mail.subject).to include "Your settings at Shrt.nr have been changed"
+    end
+
+    it "renders an html layout " do
+      expect(mail.html_part.body).to include "<p>Your settings at Shrt.nr have been changed:"
+    end
+
+  end
+
 end

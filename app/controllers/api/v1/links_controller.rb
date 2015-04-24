3032a54810f2bd560c4f05f5359837d6c8d97fb8
Scott Kostojohn <skostojohn@hotmail.com>
2015-04-24 15:52:05 -0700

    first endpoint added

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -10,4 +10,12 @@ class Api::V1::LinksController < Api::BaseController
       render json: { shorturl: full_url(@link) }
     end
   end
+  
+  def show
+    @link = Link.find(params[:id])
+    @user = User.find(@link.user_id)
+    render json:  { "short_url" => full_url(@link), "long_url" => @link.long_url, "clicks" => @link.clicks, "user" => {"name" => @user.name, "email" => @user.email}}
+  end
+  
+  
 end

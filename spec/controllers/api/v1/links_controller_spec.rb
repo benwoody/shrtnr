bc46e1e0e546f0e3e46139f602dfeded62718689
Tom Underhill <tunderhill@gmail.com>
2015-04-26 00:05:41 -0700

    added tests

--- spec/controllers/api/v1/links_controller_spec.rb
+++ spec/controllers/api/v1/links_controller_spec.rb
@@ -42,4 +42,40 @@ describe Api::V1::LinksController, type: :controller do
       end
     end
   end
+
+  describe "#show" do
+    context  "with invalid api_key" do
+      it "returns a 401" do
+        get :show, short_url:"abcdef", api_key: 'blank'
+        expect(response.code).to eq '401'
+      end
+    end
+
+    context "with a valid api_key and valid short_url" do
+      let(:json) { JSON.parse(response.body) }
+
+      before do
+        get :create, api_key: user.api_key, url: 'http://test.com'
+        short_url = JSON.parse(response.body)['shorturl'].split('/').last
+
+        get :show, short_url:short_url, api_key: user.api_key
+      end
+
+      it "returns a JSON body with the long url" do
+        expect(json['long_url']).to include 'http://test.com'
+      end
+    end
+
+    context "with a valid api_key and invalid short_url" do
+      before do
+        get :show, short_url:"abcdef", api_key: user.api_key
+      end
+
+      it "returns a JSON body with an error" do
+        expect(response.body).to include 'error'
+      end
+    end
+
+  end
+
 end

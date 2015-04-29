c1298fc9ba88695575e32ed0ad549b772e4eb887
Joseph Simpson <jjs0sbw@gmail.com>
2015-04-29 09:52:42 -0700

    Removed javascript web app test

--- spec/features/api_key_regen_spec.rb
+++ spec/features/api_key_regen_spec.rb
@@ -11,13 +11,4 @@ describe 'api_key regeneration' do
   it 'should have a api_key regeneration button' do
     expect(page).to have_button 'API Key Regeneration'
   end
-
-  describe 'should create new api key after accepting warning', js: true do
-    it 'should warn user when api key is regenerted' do
-      warning =  dismiss_confirm do
-        click_button('API Key Regeneration')
-      end
-      expect(warning).to include('Is that OK?')
-    end
-  end
 end

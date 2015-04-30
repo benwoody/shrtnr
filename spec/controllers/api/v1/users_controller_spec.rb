85e7517577b1d814b9e537a2a3e28e237b6da99a
Joseph Simpson <jjs0sbw@gmail.com>
2015-04-29 21:41:11 -0700

    Working users controller spec

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,7 +1,7 @@
 # Test specification for user controller api version 1
 require 'spec_helper'
 
-describe Api::V1::UserController, type: :controller do
+describe Api::V1::UsersController, type: :controller do
   let(:user) { create(:user) }
 
   describe '#show' do
@@ -15,15 +15,39 @@ describe Api::V1::UserController, type: :controller do
     context 'using a valid api key' do
       let(:json) { JSON.parse(response.body) }
       before do
-        get :show, api_key: user.api_key
+        get :show, api_key: user.api_key, id: user.id
       end
 
-      it 'returns a json response with a user key' do
-        expect(json['user']).not_to be_empty
+      it 'returns a JSON formated response' do
+        expect(response.content_type).to eq 'application/json'
       end
 
-      it 'shows another test' do
-        expect('1').to eq '1'
+      it 'returns a json response with a content key' do
+        expect(json.keys).not_to be_empty
+      end
+
+      it 'contains the user name' do
+        expect(json.keys).to include 'name'
+      end
+
+      it 'contains the user email' do
+        expect(json.keys).to include 'email'
+      end
+
+      it 'contains the user links' do
+        expect(json.keys).to include 'links'
+      end
+
+      it 'returns the user name' do
+        expect(json['name']).to include 'user'
+      end
+
+      it 'returns the user email' do
+        expect(json['email']).to include '.com'
+      end
+
+      it 'returns the user links' do
+        expect(json['links']).to be_kind_of(Array)
       end
     end
   end

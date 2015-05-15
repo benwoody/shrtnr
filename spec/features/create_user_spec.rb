0eb481c7a895eff49fbcf6f547a372bde97c7e90
A Kasper <alexanderjohncasper@gmail.com>
2015-05-14 20:34:18 -0700

    adds feature tests

--- spec/features/create_user_spec.rb
+++ spec/features/create_user_spec.rb
@@ -34,4 +34,38 @@ describe "creating a user" do
       expect(page).to have_content "Password confirmation doesn't match Password"
     end
   end
+
+  context "without proper email params" do
+    before do
+      visit "users/new"
+      fill_in "Email", with: ""
+      fill_in "Password", with: "Password"
+      fill_in "Password confirmation", with: "Password"
+      click_button "Submit"
+    end
+
+    it "tells the user they have made a mistake" do
+      expect(page).to have_content "You must enter an email address into the respective field"
+    end
+  end
+
+  context "with an already existing email address" do
+    before do
+      visit "users/new"
+      fill_in "Email", with: "test@test.com"
+      fill_in "Password", with: "Password"
+      fill_in "Password confirmation", with: "Password"
+      click_button "Submit"
+    end
+
+    it "tells the user they have made a mistake" do
+      visit "users/new"
+      fill_in "Email", with: "test@test.com"
+      fill_in "Password", with: "Password"
+      fill_in "Password confirmation", with: "Password"
+      click_button "Submit"
+
+      expect(page).to have_content "Email address is already registered"
+    end
+  end
 end

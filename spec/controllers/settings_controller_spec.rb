aa7e633f2dc2b1641b2e76fd84aa2825230397d4
Chris Peak <christopeak@gmail.com>
2015-07-01 08:20:02 -0700

    added a few feature and integration specs

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe SettingsController, type: :controller do
+  render_views
 
+  let(:user) { create(:user) }
+
+  before do
+    allow(self.controller).to receive(:current_user).and_return(user)
+    get :index
+  end
+
+  it 'is successfull' do
+    expect(response).to be_success
+  end
+  
+  it 'renders the index view' do
+    expect(response).to render_template("index")
+  end
 end

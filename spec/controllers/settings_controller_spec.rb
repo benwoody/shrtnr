974a7f1f19f04a15115e9bb3af282b401ff47bea
Michelle Yu <michellehyu7@gmail.com>
2015-07-08 22:58:18 -0700

    homework 1 complete

--- spec/controllers/settings_controller_spec.rb
+++ spec/controllers/settings_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'spec_helper'
 
 describe SettingsController, type: :controller do
+  let(:user) { create(:user) }
+
+  before do
+    sign_in user
+  end
+
+  describe "#index" do
+    it "is successfull" do
+      get :index
+      expect(response).to be_success
+    end
+  end
+
+  describe "#update" do
+    it "updates settings" do
+      put :update, settings: {:name=> "me", :email=> "test@test.com"}
+      expect(user[:name]).to eq("me")
+      expect(user[:email]).to eq("test@test.com")
+    end
+
+  end
 
 end
--- spec/features/create_short_url_spec.rb
+++ spec/features/create_short_url_spec.rb
@@ -2,6 +2,19 @@ require 'spec_helper'
 
 describe "creating a short url" do
   context "when not signed in" do
+    before do
+      visit "/home"
+      fill_in "long_url", with:"http://google.com"
+      click_button "Shorten It!"
+    end
+
+    it "shows user a link" do
+      expect(page).to have_content "links to"
+    end
+
+    it "contains the original link" do
+      expect(page).to have_content "http://google.com"
+    end
   end
 
   context "when signed in" do
@@ -9,6 +22,23 @@ describe "creating a short url" do
 
     before do
       login_as user
+      visit "/dashboard"
+      fill_in "long_url", with:"http://google.com"
+      click_button "Shorten It!"
     end
+
+    it "shows user a link" do
+      expect(page).to have_content "links to"
+    end
+
+    it "contains the original link" do
+      expect(page).to have_content "http://google.com"
+    end
+
+    it "shows the the link on dashboard" do
+      visit "/dashboard"
+      expect(page).to have_content "http://google.com"
+    end
+
   end
 end

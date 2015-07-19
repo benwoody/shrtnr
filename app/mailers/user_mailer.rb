41429d8a6bed37dc6265ca8000c1dfdb6b3d0f2f
Chris Peak <christopeak@gmail.com>
2015-07-19 07:07:47 -0700

    added settings mailer and specs

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: "mail@benwoodall.com"
+  default from: "christo.peak@gmail.com"
 
   def welcome_email(user)
     attachments.inline['logo.png'] = File.read("#{Rails.root}/public/assets/logo.png")
--- spec/mailers/settings_mailer_spec.rb
+++ spec/mailers/settings_mailer_spec.rb
@@ -1,20 +1,23 @@
 require "spec_helper"
 
-describe UserMailer, :type => :mailer do
+describe SettingsMailer, :type => :mailer do
   let(:user) { create(:user) }
 
-  describe "#welcome_email" do
-    let(:mail) { UserMailer.welcome_email(user) }
+  describe "#update_email" do
+    let(:mail) {SettingsMailer.update_email(user)}
 
     it "renders the correct headers" do
       expect(mail.from).to include "christo.peak@gmail.com"
       expect(mail.to).to include user.email
-      expect(mail.subject).to include "Welcome to Shortener!"
+      expect(mail.subject).to include "You updated your settings"
     end
 
     it "renders an html layout" do
       expect(mail.html_part.body).to include "You have successfully"
     end
-  end
 
+    it "renders a text layout" do
+      expect(mail.text_part.body).to include "You have updated your settings"
+    end
+  end
 end
